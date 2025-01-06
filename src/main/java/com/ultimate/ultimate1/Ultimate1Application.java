73d5e438c504e969e44803a34836520422a6b633
SJY <bangstation@naver.com>
2025-01-06 16:52:26 +0900

    add product and view product

---
+++ src/main/java/com/ultimate/ultimate1/Ultimate1Application.java
@@ -0,0 +1,13 @@
+package com.ultimate.ultimate1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ultimate1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ultimate1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ultimate/ultimate1/Ultimate1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ultimate.ultimate1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ultimate1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
